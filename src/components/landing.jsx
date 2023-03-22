9d1fcb25465c5bea262c23132634a98b1189c72f
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-03-22 23:00:55 +0530

    Added Navbar and Landing Page

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,11 +2,17 @@ import { useRef, useEffect } from "react";
 
 export default function Landing() {
     const nameREF = useRef(null);
+    const motto = useRef(null);
+    const hackInfo = useRef(null);
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const dataName = "SAMARTHYA";
     let iterations = 0;
 
     useEffect(() => {
+        motto.current.style.transform = "translateY(-30%)";
+        motto.current.style.opacity = "0";
+        hackInfo.current.style.transform = "translateY(-30%)";
+        hackInfo.current.style.opacity = "0";
         const interval = setInterval(() => {
             nameREF.current.innerHTML = nameREF.current.innerHTML
                 .split("")
@@ -20,20 +26,52 @@ export default function Landing() {
             iterations += 1 / 5;
         }, 30);
         return () => clearInterval(interval);
-    }, [nameREF]);
+    }, [nameREF, motto, hackInfo]);
 
     setTimeout(() => {
         nameREF.current.style.transition =
             "all 1s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
-        nameREF.current.style.transform = "translateY(-140%)";
-    }, 1600);
+        nameREF.current.style.transform = "translateY(-25%)";
+    }, 1800);
+
+    setTimeout(() => {
+        motto.current.className =
+            "text-2xl font-inconsolata text-zinc-400 text-center pb-8";
+        motto.current.style.transition =
+            "all 1s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
+        motto.current.style.opacity = "1";
+        motto.current.style.transform = "translateY(0.5rem)";
+    }, 2100);
+
+    setTimeout(() => {
+        hackInfo.current.className = "flex gap-4";
+        hackInfo.current.style.transition =
+            "all 1s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
+        hackInfo.current.style.opacity = "1";
+        hackInfo.current.style.transform = "translateY(0.5rem)";
+    }, 2500);
+
     return (
-        <div className="text-white h-[80vh]">
+        <div className="text-white h-[90vh]">
             <div className="flex flex-col justify-center items-center h-full ">
                 <h1 className="text-9xl font-space" ref={nameREF}>
                     SAMARTHYA
                 </h1>
-                {/* <div>29th - 30th April</div> */}
+                <h2
+                    className="text-2xl font-inconsolata text-zinc-400 text-center pb-8"
+                    ref={motto}
+                >
+                    Join the ultimate festival for developers and programmers of
+                    all skill levels! <br></br> Hack, learn, and have fun!
+                </h2>
+                <div className="flex gap-4" ref={hackInfo}>
+                    <div className="font-space text-xl bg-zinc-800 py-4 px-16 rounded-3xl text-zinc-400">
+                        29 - 30 APRIL
+                    </div>
+                    <div className="font-space text-xl bg-cyan-700 py-4 px-16 rounded-3xl text-white cursor-pointer hover:scale-105 transition ease-in-out">
+                        REGISTER FOR THE HACK
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png";
 
 export default function NavBar() {
     return (
-        <nav className="bg-light flex p-4 items-center">
+        <nav className="bg-light flex p-4 items-center sticky top-0">
             <img
                 src={logo}
                 alt="Samarthya Logo"
@@ -10,6 +10,41 @@ export default function NavBar() {
             />
             <div className="font-secular text-white text-xl pl-4">
                 Samarthya
+            </div>
+            <div className="ml-auto font-secular text-white">
+                <a
+                    href=""
+                    className="px-4  hover:text-slate-400 transition ease-in-out"
+                >
+                    HOME
+                </a>
+                <a
+                    href=""
+                    className="px-4  hover:text-slate-400 transition ease-in-out"
+                >
+                    ABOUT
+                </a>
+                <a
+                    href=""
+                    className="px-4  hover:text-slate-400 transition ease-in-out"
+                >
+                    EVENTS
+                </a>
+                <a
+                    href=""
+                    className="px-4  hover:text-slate-400 transition ease-in-out"
+                >
+                    SPONSORS
+                </a>
+                <a
+                    href=""
+                    className="px-4  hover:text-slate-400 transition ease-in-out"
+                >
+                    FAQ
+                </a>
+                <button className="px-3 py-3 bg-sky-600 rounded-xl hover:outline hover:outline-1 hover:bg-transparent transition ease-in-out hover:scale-105">
+                    REGISTER
+                </button>
             </div>
         </nav>
     );
