6056aa62a1be55c3add8203e7107038e63ad0fc3
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-03-28 20:43:51 +0530

    Cards

--- src/components/eventCard.jsx
+++ src/components/eventCard.jsx
@@ -1,7 +1,7 @@
 export default function EventCard({ title, content, date, team_number }) {
     return (
-        <div className="h-[350px] w-screen rounded-lg bg-zinc-800 mx-4 text-left pl-2 hover:scale-105 transition-all">
-            <span className="text-3xl font-space">Saviskar</span>
+        <div className="min-h-fit w-screen rounded-lg bg-zinc-800 mx-4 text-left pl-2 hover:scale-105 transition-all font-inconsolata">
+            <span className="text-3xl font-space">{title}</span>
             <div>{content}</div>
         </div>
     );
--- src/components/events.jsx
+++ src/components/events.jsx
@@ -7,11 +7,26 @@ export default function Events() {
                 EVENTS
             </div>
             <section className="Event-Card-Container flex my-16 mx-16 justify-evenly w-[90wh]">
-                <EventCard content={"Hello World"} />
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
+                <EventCard
+                    title={"Saviskar"}
+                    content={
+                        "Joust against another skilled programmer, competing to see who can solve a given problem faster. You'll need to think fast and act even faster, as you race to write the best code and beat your opponent to the finish line. It's a high-stakes battle of wits, with only one winner emerging victorious."
+                    }
+                />
+                <EventCard
+                    title={"Aiganite"}
+                    content={
+                        "An event where teams of people come together to collaborate on creating a solution to a problem or developing a project in 36 hours. Work tirelessly to develop a working prototype, pitch your ideas, and receive feedback from judges. "
+                    }
+                />
+                <EventCard
+                    title={"Devjousts"}
+                    content={
+                        "A sport where you collaborate and create your own team to compete against many other teams of competitive programmers. Ignite the logical side of your brain to find the best solution to a list of real world problems ."
+                    }
+                />
+                <EventCard title={"Crack the Code"} />
+                <EventCard title={"Figma Workshop"} />
             </section>
         </div>
     );
