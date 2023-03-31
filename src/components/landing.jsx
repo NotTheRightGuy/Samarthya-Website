42e8fd026ed5174c73692ff71f32078b6ae24f4f
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-03-31 21:39:10 +0530

    Added Backgorund video to landing page

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,10 +1,11 @@
 import { useRef, useEffect } from "react";
 import "../styles/landing.css";
-
+import landingBack from "../assets/landingBackground.mp4";
 export default function Landing() {
     const nameREF = useRef(null);
     const motto = useRef(null);
     const hackInfo = useRef(null);
+    const videoRef = useRef(null);
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const dataName = "SAMARTHYA";
     let iterations = 0;
@@ -37,7 +38,7 @@ export default function Landing() {
 
     setTimeout(() => {
         motto.current.className =
-            "text-2xl font-inconsolata text-zinc-400 text-center pb-8 max-mobile:text-base";
+            "text-2xl font-inconsolata text-white text-center pb-8 max-mobile:text-base";
         motto.current.style.transition =
             "all 1s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
         motto.current.style.opacity = "1";
@@ -45,6 +46,8 @@ export default function Landing() {
     }, 2100);
 
     setTimeout(() => {
+        videoRef.current.className =
+            "text-white h-screen blur-xl opacity-25 rounded-lg backdrop-brightness-50 transition-all duration-1000";
         hackInfo.current.className = "flex gap-4 max-mobile:gap-2";
         hackInfo.current.style.transition =
             "all 1s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
@@ -53,30 +56,50 @@ export default function Landing() {
     }, 2500);
 
     return (
-        <div id="home" className="text-white h-screen section ">
-            <div className="flex flex-col justify-center items-center h-full ">
-                <header>
-                    <h1
-                        className="heroText font-space text-9xl max-mobile:text-5xl"
-                        ref={nameREF}
-                    >
-                        SAMARTHYA
-                    </h1>
-                </header>
+        <>
+            <div
+                ref={videoRef}
+                className="text-white h-screen blur-xl opacity-0 rounded-lg backdrop-brightness-50 transition-all duration-1000"
+            >
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className="absolute object-cover w-full h-full"
+                >
+                    <source src={landingBack} />
+                </video>
+            </div>
+            <div
+                id="home"
+                className="text-white h-screen section absolute left-0 right-0  top-0"
+            >
+                <div className="flex flex-col justify-center items-center h-full ">
+                    <header>
+                        <h1
+                            className="heroText font-space text-9xl max-mobile:text-5xl"
+                            ref={nameREF}
+                        >
+                            SAMARTHYA
+                        </h1>
+                    </header>
 
-                <div ref={motto}>Unleashing Innovation, empowering future</div>
-                <div ref={hackInfo}>
-                    <div className="font-space text-xl bg-zinc-800 py-4 px-16 rounded-3xl text-zinc-400 max-mobile:px-2 max-mobile:mx-2 max-mobile:text-sm">
-                        6 - 7 MAY
+                    <div ref={motto}>
+                        Unleashing Innovation, empowering future
                     </div>
-                    <a
-                        href="#register"
-                        className="font-space text-xl max-mobile:text-sm bg-cyan-700 py-4 px-16 max-mobile:px-5 rounded-3xl text-white cursor-pointer hover:scale-105 transition ease-in-out "
-                    >
-                        REGISTER FOR THE HACK
-                    </a>
+                    <div ref={hackInfo}>
+                        <div className="font-space text-xl bg-zinc-800 py-4 px-16 rounded-3xl text-zinc-400 max-mobile:px-2 max-mobile:mx-2 max-mobile:text-sm">
+                            6 - 7 MAY
+                        </div>
+                        <a
+                            href="#register"
+                            className="font-space text-xl max-mobile:text-sm bg-cyan-700 py-4 px-16 max-mobile:px-5 rounded-3xl text-white cursor-pointer hover:scale-105 transition ease-in-out "
+                        >
+                            REGISTER FOR THE HACK
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -42,7 +42,7 @@ export default function NavBar() {
         <>
             <div
                 id="navMenu"
-                className="hidden bg-zinc-800 h-10 fixed top-0 z-50 w-full mt-14 text-center"
+                className="hidden h-10 fixed top-0 z-50 w-full mt-14 text-center"
             >
                 <button
                     className="text-zinc-400 font-space my-2 mx-2"
@@ -91,7 +91,7 @@ export default function NavBar() {
                 </button>
             </div>
             <nav
-                className="bg-[#100d11] flex p-4 items-center fixed top-0 z-50 w-screen"
+                className="bg-transparent flex p-4 items-center fixed top-0 z-50 w-screen"
                 id="mainNav"
             >
                 <img
