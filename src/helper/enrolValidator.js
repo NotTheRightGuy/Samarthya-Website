cee4cf746480f77c40d99ccc774719ef67978fad
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-04-01 17:11:09 +0530

    Fixed EnrolValidator to work with new enrolment fo

--- src/helper/enrolValidator.js
+++ src/helper/enrolValidator.js
@@ -1,4 +1,9 @@
 export default function enrolValidator(contact) {
-    const re = /^[0-9]{12}$/;
-    return re.test(String(contact));
+    if (contact.substring(0, 2) >= 22) {
+        const re = /^[0-9]{6}$/;
+        return re.test(String(contact));
+    } else {
+        const re = /^[0-9]{12}$/;
+        return re.test(String(contact));
+    }
 }
