d31374be96e91024e7e16108bb843126da719566
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-04-01 20:59:41 +0530

    changed enrolment id validator

--- src/helper/enrolValidator.js
+++ src/helper/enrolValidator.js
@@ -1,9 +1,4 @@
 export default function enrolValidator(contact) {
-    if (contact.substring(0, 2) >= 22) {
-        const re = /^[0-9]{6}$/;
-        return re.test(String(contact));
-    } else {
-        const re = /^[0-9]{12}$/;
-        return re.test(String(contact));
-    }
+    const re = /(?:\d{6}|\d{12})$/;
+    return re.test(String(contact));
 }
